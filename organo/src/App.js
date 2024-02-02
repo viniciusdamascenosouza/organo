242acb8cf8b89a1252262fdc9c38dfa6ca16c538
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-02-02 17:36:45 -0300

    componente banner

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Banner from "./componentes/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao React!
-        </p>
+        <p>Bem vindo ao React!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
