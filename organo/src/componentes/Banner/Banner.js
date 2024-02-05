bda6316eaa0cecfe188b643b59d278faa8fa7b89
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-02-05 15:35:26 -0300

    Prettier

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,11 +1,11 @@
 import "./Banner.css";
 
-function Banner () {
-    return (
-        <header className="banner">
-            <img src="/imagens/banner.png" alt="Banner organo" />
-        </header>
-    )
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="Banner organo" />
+    </header>
+  );
 }
 
 export default Banner;
