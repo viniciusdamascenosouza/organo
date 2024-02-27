f21ac8cc29581d6f3ce3e4c8b20910755e771498
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-02-27 15:52:39 -0300

    Iniciando formulário

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import "./Banner.css";
 
-function Banner() {
+export const Banner = () => {
   return (
       <header className="banner">
         <img src="/imagens/banner.png" alt="Banner organo" />
---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,11 @@
+import "./CampoTexto.css";
+
+function CampoTexto() {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder="Digite seu nome"/>
+        </div>
+    )
+}
+export default CampoTexto;
