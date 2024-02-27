190771dd0b3655d4940342b347354685ba810ecc
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-02-27 14:33:44 -0300

    banner

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -2,9 +2,9 @@ import "./Banner.css";
 
 function Banner() {
   return (
-    <header className="banner">
-      <img src="/imagens/banner.png" alt="Banner organo" />
-    </header>
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="Banner organo" />
+      </header>
   );
 }
 
