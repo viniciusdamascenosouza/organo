0f6c45095326250319c0445c283456b20e5c137a
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-04 15:51:02 -0300

    Criando formulário

---
+++ organo/src/componentes/Formulario/Formulario.js
@@ -0,0 +1,15 @@
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+
+export const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador.</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+      </form>
+    </section>
+  );
+};
