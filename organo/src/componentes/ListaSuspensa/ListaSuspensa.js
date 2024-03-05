d1dcb932aa3630574020e16fdc04ebca6cbab64d
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-05 18:33:50 -0300

    definindo times

--- organo/src/componentes/ListaSuspensa/ListaSuspensa.js
+++ organo/src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -2,7 +2,7 @@ import "./ListaSuspensa.css";
 
 export const ListaSuspensa = (props) => {
   return (
-      <div>
+      <div className="lista-suspensa">
         <label>{props.label}</label>
         <select>
           {props.itens.map(item => <option key="{item}">{item}</option>)}
