7468eefc726a7d27064a58b05295199835f4f2e0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-05 18:23:08 -0300

    Criando chave para item

--- organo/src/componentes/ListaSuspensa/ListaSuspensa.js
+++ organo/src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -5,7 +5,7 @@ export const ListaSuspensa = (props) => {
       <div>
         <label>{props.label}</label>
         <select>
-          {props.itens.map(item => <option>{item}</option>)}
+          {props.itens.map(item => <option key="{item}">{item}</option>)}
         </select>
       </div>
   );
