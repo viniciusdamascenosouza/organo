669b99da31f68828c181aca3c39c5caa9c32b9b1
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-06 15:58:48 -0300

    evento submit

--- organo/src/componentes/Formulario/Formulario.js
+++ organo/src/componentes/Formulario/Formulario.js
@@ -14,14 +14,19 @@ export const Formulario = () => {
     "Inovação e Gestão",
   ];
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("Form foi submetido");
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <CampoTexto obrigatorio="true" label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio="true" label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio="false" label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa obrigatorio="true" label="Time" itens={times} />
         <Botao>
           Criar Card
         </Botao>
--- organo/src/componentes/ListaSuspensa/ListaSuspensa.js
+++ organo/src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -4,8 +4,8 @@ export const ListaSuspensa = (props) => {
   return (
       <div className="lista-suspensa">
         <label>{props.label}</label>
-        <select>
-          {props.itens.map(item => <option key="{item}">{item}</option>)}
+        <select required="{props.required}">
+          {props.itens.map(item => <option key="item">{item}</option>)}
         </select>
       </div>
   );
