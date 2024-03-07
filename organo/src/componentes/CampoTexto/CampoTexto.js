277cefc0283690aff48d047f655ba23a5ac3c525
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-07 10:30:52 -0300

    Manipulando o input do usuário e entendendo ciclo de vida

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -2,14 +2,17 @@ import "./CampoTexto.css";
 
 export const CampoTexto = (props) => {
 
+  let valor = "Vinícius";
+
   const aoDigitado = (evento) => {
-    console.log(evento.target.value);
+    valor = evento.target.value;
+    console.log(valor);
   }
 
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input onChange={aoDigitado} required="{props.obrigatorio}" placeholder= {props.placeholder} />
+      <input value={valor} onChange={aoDigitado} required="{props.obrigatorio}" placeholder= {props.placeholder} />
     </div>
   );
 };
