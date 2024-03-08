c131cfeaca097c70629062cbb5ba42ade6d74d6d
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-08 15:06:08 -0300

    Recebendo dados do formulário em um objeto

--- organo/src/componentes/Formulario/Formulario.js
+++ organo/src/componentes/Formulario/Formulario.js
@@ -4,7 +4,7 @@ import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 import { useState } from "react";
 
-export const Formulario = () => {
+export const Formulario = (props) => {
   const times = [
     "Programação",
     "Front-end",
@@ -22,7 +22,12 @@ export const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Form foi submetido => ", nome, cargo, imagem, time);
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
