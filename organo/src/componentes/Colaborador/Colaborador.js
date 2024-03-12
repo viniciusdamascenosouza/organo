431b59713973420f9ab2262c226bbf7228a445e9
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-12 07:48:35 -0300

    Card de colaborador

---
+++ organo/src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,15 @@
+import "./Colaborador.css";
+
+export const Colaborador = (props) => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/viniciusdamascenosouza.png" alt="" />
+      </div>
+      <div className="rodape">
+        <h4>Vinícius</h4>
+        <h5>Desenvolvedor</h5>
+      </div>
+    </div>
+  );
+};
--- organo/src/componentes/Time/Time.js
+++ organo/src/componentes/Time/Time.js
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 export const Time = (props) => {
@@ -7,6 +8,7 @@ export const Time = (props) => {
   return (
     <section className="time" style={fundoTime}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <Colaborador />
     </section>
   );
 };
