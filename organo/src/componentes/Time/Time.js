49741f7587db673355ed0592a36f5070aeaaf695
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-12 06:26:35 -0300

    Estilização do título time

--- organo/src/componentes/Time/Time.js
+++ organo/src/componentes/Time/Time.js
@@ -1,9 +1,9 @@
-import 'Time.css';
+import "./Time.css";
 
 export const Time = (props) => {
-    return(
-        <section>
-            <h3>{props.nome}</h3>
-        </section>
-    )
-}
+  return (
+    <section className="time">
+      <h3>{props.nome}</h3>
+    </section>
+  );
+};
