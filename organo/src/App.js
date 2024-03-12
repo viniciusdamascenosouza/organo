10dc12813271a9902e9b05110bdd168f9480b4ac
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-12 15:41:46 -0300

    Mapeando os colaboradores

--- organo/src/App.js
+++ organo/src/App.js
@@ -53,12 +53,20 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario
-        times={times.map(time => time.nome)}
+        times={times.map((time) => time.nome)}
         aoColaboradorCadastrado={(colaborador) =>
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />
+      ))}
     </div>
   );
 }
--- organo/src/componentes/Colaborador/Colaborador.js
+++ organo/src/componentes/Colaborador/Colaborador.js
@@ -4,11 +4,11 @@ export const Colaborador = (props) => {
   return (
     <div className="colaborador">
       <div className="cabecalho">
-        <img src="https://github.com/viniciusdamascenosouza.png" alt="" />
+        <img src={props.imagem} alt={props.nome} />
       </div>
       <div className="rodape">
-        <h4>Vinícius</h4>
-        <h5>Desenvolvedor</h5>
+        <h4>{props.nome}</h4>
+        <h5>{props.cargo}</h5>
       </div>
     </div>
   );
