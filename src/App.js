7d50d92022b767e5ad4a74a90cbeec5ebbbade02
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-03-14 14:32:17 -0300

    Corrigindo problemas para deploy

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
     setColaboradores([...colaboradores, colaborador]);
   };
 
